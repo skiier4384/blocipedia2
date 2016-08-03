064e1be06a65138bdc66908080b3dadda71bd506
Andrew <rothstein.andrew@gmail.com>
2016-08-03 00:53:23 +0000

    New Project

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+ 
+ let(:user) { User.create!(name: "Blocipedia User", email: "user@blocipedia.com", password: "password") }
+   
+ it { is_expected.to have_many(:wikis) }
+ 
+ describe "attributes" do
+     it "should have name and email attributes" do
+       expect(user).to have_attributes(name: "Blocipedia User", email: "user@blocipedia.com")
+     end
+    
+ 
+end
