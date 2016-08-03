bbbcdb4ad5e681d08d76e6282e2fa19299ea2085
Andrew <rothstein.andrew@gmail.com>
2016-08-03 01:07:58 +0000

    New Project

--- app/models/user2.rb
+++ app/models/user2.rb
@@ -3,4 +3,7 @@ class User2 < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
+  before_save { self.email = email.downcase if email.present? }
+  
 end
