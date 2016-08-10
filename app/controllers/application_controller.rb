3990323c27f5dee05ffa08ded933ecb8024052bd
Andrew <rothstein.andrew@gmail.com>
2016-08-10 19:09:11 +0000

    adds default authorization exception catch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,14 @@ class ApplicationController < ActionController::Base
   #after_action :verify_authorized, except: :index
   #after_action :verify_policy_scoped, only: :index
   
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+private #--------------------------------------------------------
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  
 end
