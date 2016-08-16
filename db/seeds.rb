7fa8eac47195d1b5897cc0d98dc3897b7f26b8c2
Andrew <rothstein.andrew@gmail.com>
2016-08-16 19:18:31 +0000

    Checkpoint 6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+# Create Users
+10.times do
+    User.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password
+    )
+end
+
+# Create Admin
+unless User.find_by(email: "admin@blocipedia.com")
+  admin = User.create!(
+    name: "admin",
+    email: "admin@blocipedia.com",
+    password: "password1234",
+    )
+end
+
+users = User.all
+puts "#{User.count} users created."
+
+# Create Wikis
+20.times do
+  Wiki.create!(
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph,
+    user:  users.sample
+  )
+end
+
+wikis = Wiki.all
+puts "#{wikis.count} wikis created."
+
+puts "Seed finished"
